f5079919b37400d53a1473a9dc8e2b2cb0a4b81b
niranjana1997 <89472841+niranjana1997@users.noreply.github.com>
2022-06-11 18:35:34 -0400

    Navbar Added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 50px;
+    background-color: black;
+`
+const Navbar = () => {
+  return (
+    <Container>Navbar</Container>
+  )
+}
+
+export default Navbar
