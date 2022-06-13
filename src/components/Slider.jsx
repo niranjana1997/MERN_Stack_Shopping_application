ab39e070993c94da4f7bf68072d8e037a289509e
niranjana1997 <89472841+niranjana1997@users.noreply.github.com>
2022-06-13 14:49:01 -0400

    slider component and data file uploaded

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,44 +1,48 @@
 import React from 'react'
 import styled from 'styled-components'
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
+import {sliderItems} from "../data.js"
+import { useState } from "react";
 
 // overflow:hidden; // to prevent overflowing of content
 
 const Container = styled.div`
-    width:100%;
-    height: 100vh;
-    dislay:flex;
-    position: relative;
-    overflow:hidden;
-`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+`;
 
 // CSS - arrow used to navigate through the slider 
 const Arrow = styled.div`
-    width:50px;
-    height:50px;
-    background-color:white;
-    border-radius:50%;
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    position: absolute;
-    top:0;
-    bottom:0;
-    margin:auto;
-    left:${props => props.direction ==="left" && "10px"};
-    right:${props => props.direction ==="right" && "10px"};
-    cursor: pointer;
-    opacity: 0.5;
-`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`;
+
 
 // CSS - to move image left to right
 // diaplay: flex - to make the content vertical
 // transform:translateX(0vw) - move the slider along X-Axis
 const Wrapper = styled.div`
-    height:100%;
-    display:flex; 
-    transform:translateX(0vw);
-`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
 
 const Slide = styled.div`
     display:flex;
@@ -46,81 +50,69 @@ const Slide = styled.div`
     width:100vw;
     height:100vh;
     background-color: #${props=>props.bg}
-`
+`;
 
 const ImageContainer = styled.div`
     flex:1;
     height:100%;
-`
+`;
 const Image = styled.img`
     height: 80%;
-`
+`;
 
 const InfoContainer = styled.div`
     padding:50px;
     flex:1
-`
+`;
+
 const Title = styled.h1`
     font-size:70px;
-`
+`;
 
 const Description = styled.p`
     margin: 50px 0px;
     font-size:20px;
     font-weight:500;
     letter-spacing:2px;
-`
+`;
 
 const Button = styled.button`
     padding: 10px;
     font-size:20px;
     background-color: transparent;
     cursor:pointer;
-`
-const arrowClick = (direction) => {
-
-}
+`;
 
 export const Slider = () => {
-  return (
+    // setting 
+    const [slideIndex,setSlideIndex] = useState(0);
+    const arrowClick = (direction) => {
+        if(direction ==="left"){
+            setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2)
+        }
+        else {
+            setSlideIndex(slideIndex < 2 ? slideIndex+1 : 0)
+        }    
+    };
+    return (
     <Container>
         <Arrow direction="left" onClick={()=>arrowClick("left")}>
             <ArrowLeftOutlined/>
         </Arrow> 
-        <Wrapper>
-        <Slide bg="blue">
+        <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map(item =>(
+        <Slide bg={item.bg} key={item.id}>
         <ImageContainer>
-            <Image src="https://lp2.hm.com/hmgoepprod?set=width[1920],quality[80],options[limit]&source=url[https://www2.hm.com/content/dam/ladies_s05/april_2022/1065d/1065D-3x2-the-summer-collection.jpg]&scale=width[global.width],height[15000],options[global.options]&sink=format[jpg],quality[global.quality]"/>
+            <Image src={item.img}/>
 
         </ImageContainer>
         <InfoContainer>
-            <Title>Summer Fashion Refresh</Title>
-            <Description>Define the season in bold colors & easy, breezy silhouettes</Description>
+            <Title>{item.title}</Title>
+            <Description>{item.desc}</Description>
             <Button>Shop now</Button>
         </InfoContainer>
         </Slide>
-        <Slide>
-            <ImageContainer>
-                <Image src="https://lp2.hm.com/hmgoepprod?set=width[1920],quality[80],options[limit]&source=url[https://www2.hm.com/content/dam/ladies_s05/april_2022/1065d/1065D-3x2-the-summer-collection.jpg]&scale=width[global.width],height[15000],options[global.options]&sink=format[jpg],quality[global.quality]"/>
-
-            </ImageContainer>
-            <InfoContainer>
-                <Title>Winter Fashion Refresh</Title>
-                <Description>Define the season in bold colors & easy, breezy silhouettes</Description>
-                <Button>Shop now</Button>
-            </InfoContainer>
-            </Slide>
-            <Slide>
-            <ImageContainer>
-                <Image src="https://lp2.hm.com/hmgoepprod?set=width[1920],quality[80],options[limit]&source=url[https://www2.hm.com/content/dam/ladies_s05/april_2022/1065d/1065D-3x2-the-summer-collection.jpg]&scale=width[global.width],height[15000],options[global.options]&sink=format[jpg],quality[global.quality]"/>
-
-            </ImageContainer>
-            <InfoContainer>
-                <Title>Spring Fashion Refresh</Title>
-                <Description>Define the season in bold colors & easy, breezy silhouettes</Description>
-                <Button>Shop now</Button>
-            </InfoContainer>
-            </Slide>
+        ))}
         </Wrapper>
         <Arrow direction="right" onClick={()=>arrowClick("right")}>
             <ArrowRightOutlined/>
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://lp2.hm.com/hmgoepprod?set=width[1920],quality[80],options[limit]&source=url[https://www2.hm.com/content/dam/ladies_s05/april_2022/1065d/1065D-3x2-the-summer-collection.jpg]&scale=width[global.width],height[15000],options[global.options]&sink=format[jpg],quality[global.quality]",
+        title: "Summer fashion refresh",
+        desc: "Define the season in bold colors & easy, breezy silhouettes",
+        bg: "white",
+    },
+    {
+        id: 2,
+        img: "https://lp2.hm.com/hmgoepprod?set=width[1920],quality[80],options[limit]&source=url[https://www2.hm.com/content/dam/home_s05/may_2022/7065c/7065C-3x2-beach-towel-guide.jpg]&scale=width[global.width],height[15000],options[global.options]&sink=format[jpg],quality[global.quality]",
+        title: "Summer's best towels",
+        desc: "Affordable & fast-drying in every style & size",
+        bg: "white",
+    },
+    {
+        id: 3,
+        img: "https://lp2.hm.com/hmgoepprod?set=width[1920],quality[80],options[limit]&source=url[https://www2.hm.com/content/dam/home_s05/may_2022/7065f/7065F-3x2-kids-beach-accessories.jpg]&scale=width[global.width],height[15000],options[global.options]&sink=format[jpg],quality[global.quality]",
+        title: "Hello, sunshine!",
+        desc: "Animal capes & fun cooler bags for family beach days.",
+        bg: "white",
+    }
+]
