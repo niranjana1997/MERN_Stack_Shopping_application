5ca3cfdf33b0d1f73047b052008d494469ea600c
niranjana1997 <89472841+niranjana1997@users.noreply.github.com>
2022-06-16 15:55:23 -0400

    Register Page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 import SingleProduct from "./pages/SingleProduct";
 
 const App = () => {
   //return <div>Hello world!</div>;
-  return <SingleProduct/>;
+  return <Register/>;
   };
 
 export default App;
