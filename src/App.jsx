bcc578b917dcc8b70dd7fc0c9817594ff31c6727
niranjana1997 <89472841+niranjana1997@users.noreply.github.com>
2022-06-16 13:59:48 -0400

    Product Page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
 
 const App = () => {
   //return <div>Hello world!</div>;
-  return <Home/>;
+  return <ProductList/>;
   };
 
 export default App;
